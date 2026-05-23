086540b706b6a6e387195e319bd45ce66d34a0a5
Deva Nand <115862793+Deva-nand07@users.noreply.github.com>
2026-05-23 15:17:10 +0530

    Remove trailing slash from CORS origin URL

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const resetRoutes   = require('./routes/reset');
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://dev-pad-nine.vercel.app/'],
+  origin: ['http://localhost:5173', 'https://dev-pad-nine.vercel.app'],
   credentials: true
 }));
 app.use(express.json());
